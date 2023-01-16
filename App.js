7dfe690d922ac839af442573e60dc76e2ac91b91
gauresh123 <waranggauresh@gmail.com>
2023-01-16 13:39:11 +0530

    Hello

--- App.js
+++ App.js
@@ -1,78 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, View,Linking, TextInput, TouchableOpacity, Button,Share} from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import { Button } from 'react-native-paper'
+import Share from "react-native-share";
 
+function App() {
 
-
-export default function App() {
-  const[num,setNum]=useState("9786241276");
-  const[whmsg,setWhmsg]=useState(
-    "Please Click On a Link : https://en.wikipedia.org/wiki/Shivaji"
-  )
-
-  const pressHandel = (()=>{
-    let url = 'whatsapp://send?text='+whmsg+'&phone=91'+num;
-    Linking.openURL(url)
-    .then((data)=>{
-      console.log("whatsapp opened")
-    }).catch((err)=>{
-      alert("make sure whatsapp installed")
-    })
-  })
-
-  {/*const pressHandele = (()=>{
-    const shareoptions={
-      title:"Share Via",
-      message:"Hello",
-      url:"https://en.wikipedia.org/wiki/Shivaji",
-      social: "whatsapp",
-      whatsAppNumber:"919967589255",
-      filename:"test"
+  const sharelink = (()=>{
+    const options={
+      url:"https://simple.wikipedia.org/wiki/Main_Page",
+      message:"Hello"
     }
-
-    Share.shareSingle(shareoptions)
-    .then((res)=>{console.log(res)})
-    .catch((err)=>{err && console.log(err)})
-    
-    
-  })*/}
-
-
+    Share.open(options);
+  })
   return (
-    <View style={styles.container}>
-     <Text style={{fontWeight:"600",fontSize:18,marginBottom:"5%"}}> To send Massege to Mobile Number</Text>
-     
-      <TouchableOpacity onPress={pressHandel} style={styles.touch}>
-        <Text style={{color:"white"}}>Send Massege</Text>
-      </TouchableOpacity>
-
-     {/* <TouchableOpacity onPress={pressHandele} style={styles.touch}>
-        <Text style={{color:"white"}}>Send </Text>
-  </TouchableOpacity>*/}
-  
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Button mode='contained' onPress={sharelink}>Share Me</Button>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input:{
-    width:"80%",
-    height:60,
-    borderWidth:1,
-    padding:12
-  },
-  touch:{
-    marginTop:"8%",
-    width:"80%",
-    height:50,
-    padding:12,
-    alignItems:"center",
-    backgroundColor:"green"
-  }
-});
+export default App
